c6f0ec292c98f9b81c717d7b7014e1160f7632f5
Adam Benedek <benedek2007@hotmail.com>
2020-12-03 19:42:34 +0000

    Add client side picture capture

---
+++ timelapse-client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p -v ./temporaryStorage/
+
+fileName=./temporaryStorage/$(date '+%Y-%m-%d-%H-%M-%S').jpg
+
+echo 'Saving picture at' $fileName 
+touch $fileName
+
+if [[ $(jq -r .useRemoteHost config.json) == "false" ]]
+then
+    permanentLocalStorage=$(jq -r .permanentLocalStorage config.json)
+    if [[ $permanentLocalStorage == "null" ]]
+    then
+        echo 'You need to define permanentLocalStorage in config.json. Exiting...'
+        exit 1
+    else
+        mkdir -p -v $permanentLocalStorage
+        echo 'Copying files from ./temporaryStorage/ to ' $permanentLocalStorage
+        cp -a ./temporaryStorage/. $permanentLocalStorage
+    fi
+fi

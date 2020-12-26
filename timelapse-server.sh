5b0fe864f9d77e70083bafafe7b114e8eef18dea
Adam Benedek <benedek2007@hotmail.com>
2020-12-26 22:02:59 +0000

    Fix video creation

--- timelapse-server.sh
+++ timelapse-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ../permanentTimelapseStorage && ffmpeg -framerate 10 -pattern_type glob -i '*.jpg' -c:v libx264 out.mp4
+cd ../permanentTimelapseStorage && ffmpeg -y -framerate 10 -pattern_type glob -i '*.jpg' -c:v libx264 out.mp4

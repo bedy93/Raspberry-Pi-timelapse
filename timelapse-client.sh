d88312064b133845b6c7e48af0f1f0b6fcad847f
Adam Benedek <benedek2007@hotmail.com>
2020-12-29 20:44:34 +0000

    Add sunset, sunrise checking to avoid night pictures

--- timelapse-client.sh
+++ timelapse-client.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# Insert your location. For example HUXX0017 is a location code for Nagykanizsa, Hungary
+location="HUXX0017"
+tmpfile=/tmp/$location.out
+
+# Obtain sunrise and sunset raw data from weather.com
+wget -q "https://weather.com/weather/today/l/$location" -O "$tmpfile"
+
+SUNR=$(grep SunriseSunset "$tmpfile" | grep -oE '((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))' | head -1)
+SUNS=$(grep SunriseSunset "$tmpfile" | grep -oE '((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))' | tail -1)
+
+sunrise=$(date --date="$SUNR" +%R)
+sunset=$(date --date="$SUNS" +%R)
+
+# Use $sunrise and $sunset variables to fit your needs. Example:
+echo "Sunrise for location $location: $sunrise"
+echo "Sunset for location $location: $sunset"
+
+currentTime=$(date +%H%M)
+echo "Current time: $currentTime"
+# Checking whether sun is up, to avoid dark images. 1 hour offset is used.
+if [[ $currentTime -lt $((10#${sunrise//:} - 100)) || $currentTime -gt $((10#${sunset//:} + 100)) ]];
+then
+    echo 'It is night. Exiting...'
+    exit 0
+fi
+
 mkdir -p -v ./temporaryStorage/
 
 fileName=./temporaryStorage/$(date '+%Y-%m-%d-%H-%M-%S').jpg

badf48f91c5beb764b76a3b3bb3f23f3ca255bc7
Adam Benedek <benedek2007@hotmail.com>
2021-01-17 21:03:44 +0000

    Change resolution

--- timelapse-server.sh
+++ timelapse-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ../permanentTimelapseStorage && ffmpeg -y -framerate 10 -pattern_type glob -i '*.jpg' -c:v libx264 out.mp4
+cd ../permanentTimelapseStorage && ffmpeg -y -framerate 10 -pattern_type glob -i '*.jpg' -c:v libx264 -s 800x600 out.mp4

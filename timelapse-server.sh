126c9e06022b23224d54bf48a5189574e83e9b62
Adam Benedek <benedek2007@hotmail.com>
2021-01-19 18:50:51 +0000

    Remove daytime condition for video generation

--- timelapse-server.sh
+++ timelapse-server.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-source ./supportFunctions.sh
-
-exitAtNightTime "HUXX0017" 100
 
 cd ../permanentTimelapseStorage && ffmpeg -y -framerate 10 -pattern_type glob -i '*.jpg' -c:v libx264 -s 800x600 out.mp4

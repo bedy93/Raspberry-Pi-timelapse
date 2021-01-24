c6dd6cc25c1a665aec731e72a6a273cef43ff150
Adam Benedek <benedek2007@hotmail.com>
2021-01-24 19:21:26 +0000

    Add location to config for day/night decesion, remove temporary saving of image.

--- timelapse-client.sh
+++ timelapse-client.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 source ./supportFunctions.sh
 
-exitAtNightTime "HUXX0017" 100
+if [[ $(jq -r .createTimelapseOnlyDurinDayTime config.json) == "true" ]]
+then
+    locationCode=$(jq -r .locationCode config.json)
+    exitAtNightTime $locationCode 100
+fi
 
 mkdir -p -v ./temporaryStorage/
-
-fileName=./temporaryStorage/$(date '+%Y-%m-%d-%H-%M-%S').jpg
-
-echo 'Saving picture at' $fileName
-raspistill -o $fileName
 
 if [[ $(jq -r .useRemoteHost config.json) == "false" ]]
 then
@@ -33,8 +32,15 @@ then
         echo 'You need to define remoteHost and remoteHostUserName in config.json. Exiting...'
         exit 1
     else
-        echo 'Copying files from ./temporaryStorage/ to ' $remoteHostUserName@$remoteHost/permanentTimelapseStorage
-        $(rsync -a -e "ssh -i $remoteHostUserName" ./temporaryStorage/ $remoteHostUserName@$remoteHost:permanentTimelapseStorage)
+        echo 'Saving image remotely at' $remoteHostUserName@$remoteHost/permanentTimelapseStorage
+        resultOfUploading=$(raspistill -o - | ssh -i $remoteHostUserName $remoteHostUserName@$remoteHost 'cat > ~/permanentTimelapseStorage/$(date '+%Y-%m-%d-%H-%M-%S').jpg' 2>&1 >/dev/null) 
+        echo $resultOfUploading
+        if [[ $resultOfUploading ]]
+        then
+            fileName=./temporaryStorage/$(date '+%Y-%m-%d-%H-%M-%S').jpg
+            echo 'Saving image to remote host was not successful. Saving it locally at' $fileName
+            raspistill -o $fileName
+        fi
         echo 'Finished. Exiting...'
         exit 0 
     fi

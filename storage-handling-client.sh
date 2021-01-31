94881f7aeead63ed9a4a5062cd9680385a9e0dba
Adam Benedek <benedek2007@hotmail.com>
2021-01-31 19:50:26 +0000

    Remove size condition for folder syncing

--- storage-handling-client.sh
+++ storage-handling-client.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-tempStorageDeletionSizeLimitInMB=$(jq -r .tempStorageDeletionSizeLimitInMB config.json)
-if [[ $tempStorageDeletionSizeLimitInMB == "null" ]]
-then
-    echo 'You need to define tempStorageDeletionSizeLimitInMB in config.json. Exiting...'
-    exit 1
-fi
-
 if [ ! -d "./temporaryStorage" ]
 then
     echo 'temporaryStorage does not exist. Exiting'
     exit 1
-fi
-
-echo 'Cheking size of ./temporaryStorage'
-directorySize=$(du -s ./temporaryStorage | grep -o '[0-9]\+')
-directorySize=$(expr $directorySize / 1000)
-echo 'TemporaryStorage size:' $directorySize 'MB'
-echo 'Directory deletion threshold :' $tempStorageDeletionSizeLimitInMB 'MB'
-
-if [ $tempStorageDeletionSizeLimitInMB -gt $directorySize ]
-then
-    echo 'Temp directory did not reach given size. Exiting'
-    exit 0
 fi
 
 echo 'Saving everything before deletion...'

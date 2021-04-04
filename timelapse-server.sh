5fd948683871d1d7004aedcc30e9d47a03a4dc5a
Adam Benedek <benedek2007@hotmail.com>
2021-04-04 15:35:29 +0000

    Adapt video generation for folder based structure

--- timelapse-server.sh
+++ timelapse-server.sh
@@ -2,5 +2,20 @@
 
 resolution=$(jq -r .timelapseVideoResolution config.json)
 
-cd ../permanentTimelapseStorage && ffmpeg -y -framerate 10 -pattern_type glob -i '*.jpg' -c:v libx264 -s $resolution out.mp4
+cd ../permanentTimelapseStorage
+rm listOfVideos.txt
 
+for dir in */     # list directories
+do
+    dir=${dir%*/}      # remove the trailing "/"
+    if test -f "$dir/out.mp4";
+    then
+        echo "Video for $dir exists."
+    else
+        echo "Generating video $dir."
+    	ffmpeg -y -framerate 10 -pattern_type glob -i $dir'/*.jpg' -c:v libx264 -s $resolution $dir/out.mp4
+    fi
+    echo "file '"$dir"/out.mp4'" >> listOfVideos.txt
+done
+
+ffmpeg -y -f concat -safe 0 -i listOfVideos.txt -c copy out.mp4 #Create the full video with concatenation
